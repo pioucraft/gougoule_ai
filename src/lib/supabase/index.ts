03ac00a15c405e2871609ba962072eb98be95858
Pfannkuchen <hello@gougoule.ch>
2024-06-07 20:11:05 +0200

    add log in

---
+++ src/lib/supabase/index.ts
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+// Create a single supabase client for interacting with your database
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+export const supabase = createClient(supabaseUrl, supabaseKey)
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<input bind:value={email} type="text" placeholder="username">
+<input bind:value={password} type="password" placeholder="password">
+<button on:click={login}>Log in</button>
+
+<script>
+    
+
+    var email = ""
+    var password = ""
+
+    import { supabase } from "$lib/supabase";
+
+    async function login() {
+        const { data, error } = await supabase.auth.signInWithPassword({
+            email: email,
+            password: password,
+        })
+        console.log(data, error)
+    }
+</script>
