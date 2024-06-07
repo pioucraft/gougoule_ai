5f58021e8167e8bad89849c442bf0a8916cdac65
Pfannkuchen <hello@gougoule.ch>
2024-06-07 20:18:01 +0200

    remove console.log

--- src/lib/supabase/index.ts
+++ src/lib/supabase/index.ts
@@ -3,5 +3,4 @@ import { createClient } from '@supabase/supabase-js'
 // Create a single supabase client for interacting with your database
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
-console.log(import.meta.env.VITE_SUPABASE_ANON_KEY)
 export const supabase = createClient(supabaseUrl, supabaseKey)
