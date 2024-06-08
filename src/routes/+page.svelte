c785d285b751d4943f52cacadcf1796381cefa94
Pfannkuchen <hello@gougoule.ch>
2024-06-08 17:43:24 +0200

    add page.svelte

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import { supabase } from "$lib/supabase";
+	import { onMount } from "svelte";
+
+    onMount(() => {
+        console.log(supabase.functions.invoke("ask"))
+    })
+    
+</script>
